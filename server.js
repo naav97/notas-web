d3cee51c64ebf548dd1873cfc5b003167f3c4584
na.alvarado <na.alvarado@uniandes.edu.co>
2020-12-03 18:31:41 -0500

    ya casito

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
+if(process.env.NODE_ENV !== 'production'){
+	require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initialize = require('./passport-config')
+
+initialize(passport, 
+	email => users.find(user => user.email === email),
+	id => users.find(user => user.id === id)
+)
+
+const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(flash())
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialize: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/', (req, res) => {
-	res.render('index.ejs', { name: 'Nicolas' })
+app.get('/', checkAuthenticated, (req, res) => {
+	res.render('index.ejs', { name: req.user.name })
 })
 
 app.get('/login', (req, res) => {
@@ -14,5 +40,33 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
 	res.render('register.ejs')
 })
+
+app.post('/register', async (req, res) => {
+	try{
+		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+		users.push({
+			id: Date.now().toString(),
+			name: req.body.name,
+			email: req.body.email,
+			password: hashedPassword
+		})
+		res.redirect('/login')
+	} catch {
+		res.redirect('/register')
+	}
+})
+
+app.post('/login', passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/login',
+	failureFlash: true
+}))
+
+function checkAuthenticated(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next()
+	}
+	res.redirect('/login')
+}
 
 app.listen(3000)

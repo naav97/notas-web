87468b74b4be524881b66e9eaec1b638a35708b3
na.alvarado <na.alvarado@uniandes.edu.co>
2020-12-16 11:18:48 -0500

    ok ok starting to get somewere

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var clase = require('../clase');
+
+var clases = [
+	{
+		"nombre":"Calculo",
+		"notas":[]
+	},
+	{
+		"nombre":"Fisica",
+		"notas":[]
+	},
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Notas Web', clases: clases });
 });
 
 module.exports = router;

579a393b69fbf4f6ac0d5f2c3ebe9407555463f4
na.alvarado <na.alvarado@uniandes.edu.co>
2020-12-17 09:21:35 -0500

    gets for / and /clase

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,20 @@ var clase = require('../clase');
 var clases = [
 	{
 		"nombre":"Calculo",
-		"notas":[]
+		"notas":[
+			{
+				"nombre":"Parcial1",
+				"porcentage":20,
+				"puntos":0,
+				"obtenida":false,
+			},
+			{
+				"nombre":"Parcial2",
+				"porcentage":20,
+				"puntos":0,
+				"obtenida":false,
+			}
+		]
 	},
 	{
 		"nombre":"Fisica",
@@ -16,6 +29,12 @@ var clases = [
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Notas Web', clases: clases });
+});
+
+router.get('/:nombre', function(req, res, next) {
+	const cl = clases.filter(clase => clase.nombre === req.params.nombre);
+	//res.json(cl[0])
+	res.render('clase', {clase: cl[0]});
 });
 
 module.exports = router;
